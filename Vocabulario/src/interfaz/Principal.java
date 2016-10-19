b33f281bad42bbc7ffdebaf7241601564d37328b
pablo657 <pablo657@hotmail.com>
2016-10-18 21:46:59 -0300

    principal provisorio

---
+++ Vocabulario/src/interfaz/Principal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaz;
+
+import java.io.File;
+import negocio.Vocabulario;
+
+/**
+ *
+ * @author pablo
+ */
+public class Principal
+{
+    public static void main(String[] args)
+    {
+         File arch = new File("C:\\Users\\pablo\\Desktop\\TSB 2016\\00 TRABAJO PRACTICO\\16082-8.txt");
+         
+         Vocabulario voc = new Vocabulario(arch);
+         voc.leerArchivo(arch);
+    }
+    
+}
