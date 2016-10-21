ea7474fc90a9184364947f67b483aaba810ca597
pablo657 <pablo657@hotmail.com>
2016-10-21 19:43:31 -0300

    gif

---
+++ Vocabulario/src/soporte/WorkerCargar.java
@@ -0,0 +1,19 @@
+
+package soporte;
+
+import javax.swing.SwingWorker;
+
+/**
+ *
+ * @author pablo
+ */
+public class WorkerCargar extends SwingWorker<Boolean, Double>
+{
+
+    @Override
+    protected Boolean doInBackground() throws Exception
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
