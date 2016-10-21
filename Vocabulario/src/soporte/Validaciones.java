be45c2d3c95c9b9156630021f5b1bed3ff3b6ad3
haploide <haploide1998@hotmail.com>
2016-10-21 19:06:14 -0300

    filtro finalizado!!!

--- Vocabulario/src/soporte/Validaciones.java
+++ Vocabulario/src/soporte/Validaciones.java
@@ -10,7 +10,7 @@ public class Validaciones
     
     public static boolean esTexto(char st)
     {
-        return Pattern.matches("[a-zA-Z]", st+"");
+        return !Pattern.matches("[0-9 \\p{Punct}]", st+"");
     }
     public static boolean estaVacio(String st)
     {
