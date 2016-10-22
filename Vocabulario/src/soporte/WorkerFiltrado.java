cbf4f6bff4d087311d8b5f29672d7f39ae9ec935
haploide <haploide1998@hotmail.com>
2016-10-21 23:17:23 -0300

    worker hdp

---
+++ Vocabulario/src/soporte/WorkerFiltrado.java
@@ -0,0 +1,57 @@
+package soporte;
+
+
+import java.util.ArrayList;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.SwingWorker;
+import negocio.Palabra;
+
+public class WorkerFiltrado extends SwingWorker<ArrayList<Palabra>, Void>
+{
+
+    private final JLabel jlblGif;
+    private final JLabel jlblResultado;
+    private final JLabel jLlbCantidad;
+    private final String filtro;
+    private ArrayList<Palabra> byFilterPalabras;
+    private final JTable jTblGrillaPalabras;
+
+    public WorkerFiltrado(JLabel jlblGif, JLabel jlblResultado, ArrayList<Palabra> byFilterPalabras, JLabel jLlbCantidad, String filtro, JTable jTblGrillaPalabras)
+    {
+        this.jlblGif = jlblGif;
+        this.jlblResultado = jlblResultado;
+        this.jLlbCantidad = jLlbCantidad;
+        this.filtro = filtro;
+        this.jTblGrillaPalabras = jTblGrillaPalabras;
+        this.byFilterPalabras=byFilterPalabras;
+    }
+
+    
+
+    
+
+    @Override
+    protected ArrayList<Palabra> doInBackground() throws Exception
+    {
+        jlblResultado.setText("Filtrando...");
+        jlblGif.setVisible(true);
+        byFilterPalabras= Persistencia.getByFilterPalabras(filtro);
+              
+        return byFilterPalabras;
+
+    }
+
+    @Override
+    public void done()
+    {
+        jlblGif.setVisible(false);
+        jlblResultado.setText("Filtrado Finalizado!");
+        jLlbCantidad.setText("Cantidad de Elementos Filtrados: " + byFilterPalabras.size());
+        jTblGrillaPalabras.updateUI();
+        
+        
+
+    }
+
+}
